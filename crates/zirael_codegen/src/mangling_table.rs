5e9b9412846706eeb5bbbc6e946be0568c072246
malezjaa <poreba.kris@gmail.com>
2025-09-21 14:32:33 +0200

    add type paramaters to monomorphized mangled names

--- crates/zirael_codegen/src/mangling_table.rs
+++ crates/zirael_codegen/src/mangling_table.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
-use zirael_parser::{MonomorphizationId, OriginalSymbolId, Path, Symbol, SymbolId, SymbolTable};
-use zirael_type_checker::{GenericSymbol, MonoSymbolTable};
+use zirael_parser::{MonomorphizationId, OriginalSymbolId, SymbolId, SymbolTable};
+use zirael_parser::ty::{Ty, TyId};
+use zirael_type_checker::{GenericSymbol, GenericSymbolKind, MonoSymbolTable, MonomorphizedSymbol};
 use zirael_utils::prelude::{Sources, get_or_intern, resolve, strip_same_root};
 
 #[derive(Debug)]
@@ -63,8 +64,8 @@ impl<'table, 'sources> ManglingTable<'table, 'sources> {
   }
 
   pub fn get_module_path(&self, sym: &GenericSymbol) -> PathBuf {
-    let canonical_id = self.symbol_table.get_symbol_unchecked(&sym.symbol_id()).canonical_symbol;
-    let canonical_symbol = self.symbol_table.get_symbol_unchecked(&canonical_id);
+    let canonical_id = self.symbol_table.get_symbol(sym.symbol_id()).unwrap().canonical_symbol;
+    let canonical_symbol = self.symbol_table.get_symbol(canonical_id).unwrap();
 
     let symbol_file = self.symbol_table.get_symbol_module(canonical_symbol.scope).unwrap();
     let file_path = self.sources.get_unchecked(symbol_file).path();
@@ -106,7 +107,17 @@ impl<'table, 'sources> ManglingTable<'table, 'sources> {
       let generic = self.sym_table.get_generic_symbol(mono.original_symbol_id()).unwrap();
       let mod_path = self.get_module_segment(generic);
 
-      let base = self.get_base_name(&generic);
+      let mut base = self.get_base_name(&generic);
+
+      if let Some(type_args) = self.get_ordered_type_arguments(&generic, &mono) {
+        if !type_args.is_empty() {
+          let type_suffix = type_args.iter()
+            .map(|ty_id| self.mangle_ty_for_name(*ty_id))
+            .collect::<Vec<_>>()
+            .join("_");
+          base.push_str(&format!("__{}", type_suffix));
+        }
+      }
 
       self.sym_table.add_mangled_name(OriginalSymbolId::Monomorphization(variant), mod_path, base);
     }
@@ -118,5 +129,84 @@ impl<'table, 'sources> ManglingTable<'table, 'sources> {
     let mod_path = self.get_module_segment(sym);
 
     self.sym_table.add_mangled_name(OriginalSymbolId::Symbol(symbol), mod_path, base);
+  }
+
+  fn get_ordered_type_arguments(&self, generic: &GenericSymbol, mono: &MonomorphizedSymbol) -> Option<Vec<TyId>> {
+    let generics = match &generic.kind {
+      GenericSymbolKind::Function { generics, .. } => generics,
+      GenericSymbolKind::Struct { generics, .. } => generics,
+      GenericSymbolKind::Enum { generics, .. } => generics,
+      _ => return None,
+    };
+
+    if generics.is_empty() {
+      return None;
+    }
+
+    let concrete_types = mono.concrete_types();
+    let type_args: Vec<TyId> = generics
+      .iter()
+      .filter_map(|param| {
+        concrete_types.get(&param.name).copied()
+      })
+      .collect();
+
+    if type_args.len() == generics.len() {
+      Some(type_args)
+    } else {
+      None
+    }
+  }
+
+  fn mangle_ty_for_name(&self, ty_id: TyId) -> String {
+    let ty = self.sym_table.resolve(ty_id);
+    match ty {
+      Ty::String => "string".to_owned(),
+      Ty::Char => "char".to_owned(),
+      Ty::Int => "int".to_owned(),
+      Ty::Uint => "uint".to_owned(),
+      Ty::Float => "float".to_owned(),
+      Ty::Bool => "bool".to_owned(),
+      Ty::Void => "void".to_owned(),
+      Ty::Never => "never".to_owned(),
+      Ty::Pointer(inner) => {
+        let inner_id = self.sym_table.lookup(inner).unwrap_or_else(|| {
+          panic!("Could not find TyId for inner pointer type: {:?}", inner)
+        });
+        format!("ptr{}", self.mangle_ty_for_name(inner_id))
+      }
+      Ty::Reference(inner) => {
+        let inner_id = self.sym_table.lookup(inner).unwrap_or_else(|| {
+          panic!("Could not find TyId for inner reference type: {:?}", inner)
+        });
+        format!("ref{}", self.mangle_ty_for_name(inner_id))
+      }
+      Ty::Array(inner) => {
+        let inner_id = self.sym_table.lookup(inner).unwrap_or_else(|| {
+          panic!("Could not find TyId for inner array type: {:?}", inner)
+        });
+        format!("slice{}", self.mangle_ty_for_name(inner_id))
+      }
+      Ty::Function(params, ret) => {
+        let param_names = params.iter()
+          .map(|p| {
+            let p_id = self.sym_table.lookup(p).unwrap_or_else(|| {
+              panic!("Could not find TyId for function param type: {:?}", p)
+            });
+            self.mangle_ty_for_name(p_id)
+          })
+          .collect::<Vec<_>>()
+          .join("_");
+        let ret_id = self.sym_table.lookup(ret).unwrap_or_else(|| {
+          panic!("Could not find TyId for function return type: {:?}", ret)
+        });
+        format!("fn_{}__ret_{}", param_names, self.mangle_ty_for_name(ret_id))
+      }
+      Ty::Symbol(original_id) => match original_id {
+        OriginalSymbolId::Symbol(sym_id) => format!("sym_{}", sym_id.index()),
+        OriginalSymbolId::Monomorphization(mono_id) => format!("mono_{}", mono_id.index()),
+      },
+      Ty::GenericVariable { name, .. } => format!("var_{}", resolve(name)),
+    }
   }
 }
